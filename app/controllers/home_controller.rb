72bc5a450abe787082b027116b929c7db09c3137
HA Quang Minh <minhprg@gmail.com>
2014-11-01 11:57:44 +0700

    Added a default sample template as a reference

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def categories
+
+  end
+
+  def details
+
+  end
+
+  def search
+
+  end
+
+  def sample
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   get 'home/index'
+  get 'home/sample'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
