b595917a7952abecd26bf8dda8e2f44a76859400
nodame chan <dungconga93@gmail.com>
2014-12-18 21:14:45 +0700

    update all views, and home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,24 +11,28 @@ class HomeController < ApplicationController
   def phuchung
     @can_dai=Art.all.where('category = "Cận đại"').order('created_at DESC').first
     @duong_dai=Art.all.where('category = "Đương đại"').order('created_at DESC').first
-
     @list_arts=Art.all.where('category = "Phục hưng"').paginate(:page => params[:page], :per_page => 5)
   end
   def candai
     @phuc_hung=Art.all.where('category = "Phục hưng"').order('created_at DESC').first
     @duong_dai=Art.all.where('category = "Đương đại"').order('created_at DESC').first
-
-    @list_arts=Art.all.where('category = "Cận đại"').paginate(:page => params[:page], :per_page => 5)
+    @list_arts_candai=Art.all.where('category = "Cận đại"').paginate(:page => params[:page], :per_page => 5)
 
   end
   def duongdai
     @phuc_hung=Art.all.where('category = "Phục hưng"').order('created_at DESC').first
     @can_dai=Art.all.where('category = "Cận đại"').order('created_at DESC').first
-    @list_arts=Art.all.where('category = "Cận đại"').paginate(:page => params[:page], :per_page => 5)
+    @list_arts_duongdai=Art.all.where('category = "Đương đại"').paginate(:page => params[:page], :per_page => 5)
   end
   def single
     @article =Art.find(params[:id])
-    @list_arts=Art.all.where('category = "Phục hưng"')
+    if(@article.category=="Phục hưng")
+      @list_arts=Art.all.where('category = "Phục hưng"')
+    elsif(@article.category=="Cận đại")
+      @list_arts=Art.all.where('category = "Cận đại"')
+    else
+      @list_arts=Art.all.where('category = "Đương đại"')
+    end
   end
   def search
 
